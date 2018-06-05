b497ee4f6047435ec7da0bacf41abb3c5f0bd2aa
IgorColetoBueno <igor_mcb@hotmail.com>
2018-06-05 19:48:05 -0400

    Alteração de teste de integração com a equipe.

--- br.com.Achaeu/src/DAO/AvaliacaoDAO.java
+++ br.com.Achaeu/src/DAO/AvaliacaoDAO.java
@@ -90,7 +90,7 @@ public class AvaliacaoDAO implements IRepository {
 
     @Override
     public IEntidade obterUm(Integer id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return null;
     }
 
     @Override
