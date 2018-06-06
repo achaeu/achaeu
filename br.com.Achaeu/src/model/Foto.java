e8a15e5046a89b36e47ff82fae670002244b6b68
IgorColetoBueno <igor_mcb@hotmail.com>
2018-06-05 20:54:07 -0400

    Tentativa de correção de arquivos a ignorar

--- br.com.Achaeu/src/model/Foto.java
+++ br.com.Achaeu/src/model/Foto.java
@@ -3,15 +3,18 @@ package model;
 public class Foto extends IEntidade{
     private String diretorioAbsoluto;
     private Integer idLocal;
+    private Local local;
 
     public Foto() {
         super();
+        this.local = new Local();
     }    
-    
-    public Foto(Integer id, String diretorioAbsoluto, Integer idLocal) {
+
+    public Foto(String diretorioAbsoluto, Integer idLocal, Local local, Integer id) {
         super(id);
         this.diretorioAbsoluto = diretorioAbsoluto;
         this.idLocal = idLocal;
+        this.local = local;
     }
 
     public String getDiretorioAbsoluto() {
@@ -30,8 +33,16 @@ public class Foto extends IEntidade{
         this.idLocal = idLocal;
     }
 
+    public Local getLocal() {
+        return local;
+    }
+
+    public void setLocal(Local local) {
+        this.local = local;
+    }
+
     @Override
     public String toString() {
-        return "Foto{" + "id=" + super.getId() + ", diretorioAbsoluto=" + diretorioAbsoluto + ", idLocal=" + idLocal + '}';
+        return "Foto{" + "diretorioAbsoluto=" + diretorioAbsoluto + ", idLocal=" + idLocal + ", local=" + local + '}';
     }    
 }
