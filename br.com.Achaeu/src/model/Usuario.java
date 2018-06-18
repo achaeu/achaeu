c7a7ab40c6cc6afd4c7c09d04ccff9ad1d8ccafd
Jonathan Lazzaro <jonathan.lazaro1@gmail.com>
2018-06-18 10:48:13 -0400

    Criado enum para nível de usuário.

--- br.com.Achaeu/src/model/Usuario.java
+++ br.com.Achaeu/src/model/Usuario.java
@@ -1,12 +1,13 @@
 package model;
 
 import java.security.Timestamp;
+import model.enums.UsuarioNivel;
 
 public class Usuario extends IEntidade{
     private String nome;
     private String email;
     private String senha;
-    private int nivel;
+    private UsuarioNivel nivel;
     private Timestamp dataCriacao;
     private Timestamp dataAlteracao;
 
@@ -18,7 +19,7 @@ public class Usuario extends IEntidade{
         this.nome = nome;
         this.email = email;
         this.senha = senha;
-        this.nivel = nivel;
+        this.nivel = UsuarioNivel.values()[nivel];
         this.dataCriacao = dataCriacao;
         this.dataAlteracao = dataAlteracao;
     }
@@ -47,12 +48,12 @@ public class Usuario extends IEntidade{
         this.senha = senha;
     }
 
-    public int getNivel() {
+    public UsuarioNivel getNivel() {
         return nivel;
     }
 
     public void setNivel(int nivel) {
-        this.nivel = nivel;
+        this.nivel = UsuarioNivel.values()[nivel];
     }
 
     public Timestamp getDataCriacao() {
