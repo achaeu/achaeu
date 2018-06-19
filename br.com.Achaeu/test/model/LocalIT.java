4c125f20df0f601578ae58763c63e64d8f032643
Unknown <rodrigofelippeac@gmail.com>
2018-06-18 22:59:41 -0400

    Adicionado os testes

--- br.com.Achaeu/test/model/LocalIT.java
+++ br.com.Achaeu/test/model/LocalIT.java
@@ -19,40 +19,38 @@ import org.junit.Ignore;
  * @author Rodrigo
  */
 public class LocalIT {
-    
+
     public LocalIT() {
     }
 
-@Test
+    @Test
     @Ignore
     public void inserir() {
-        
+
         Local l1 = new Local();
         LocalDAO dao1 = new LocalDAO();
-        
+
         l1.setNome("Local teste");
         l1.setDescricao("TEste de Criação de DAO");
         l1.setIdUsuario(2);
         l1.setIdEndereco(1);
         l1.setIdCategoria(1);
-        
-        
-        if (dao1.inserir(l1).getId() != 0 ){
+
+        if (dao1.inserir(l1).getId() != 0) {
             System.out.println("Inserido com sucesso!");
         } else {
-            System.out.println("Erro ao inserir"); 
+            System.out.println("Erro ao inserir");
         }
     }
-    
-    
+
     @Test
-    //@Ignore
-    public void alterar(){
-        
+    @Ignore
+    public void alterar() {
+
         Local l1 = new Local();
         LocalDAO da1 = new LocalDAO();
         System.out.println("Teste 2");
-        
+
         // Obtem o que deseja alterar 
         l1 = (Local) da1.obterUm(5);
         // Altera o local
@@ -63,26 +61,42 @@ public class LocalIT {
 
     @Test
     @Ignore
-    public void ObterTodos(){
-      
+    public void ObterTodos() {
+
         Local l1 = new Local();
         LocalDAO da1 = new LocalDAO();
         List<IEntidade> fotos = new ArrayList<>();
-        
+
         fotos = da1.obterTodos();
-        
-        for(IEntidade avals : fotos){
-            System.out.println(avals.getId()+" ");
+
+        for (IEntidade avals : fotos) {
+            System.out.println(avals.getId() + " ");
         }
-        
+
     }
-    
+
     @Test
     @Ignore
-    public void remover(){
+    public void remover() {
         LocalDAO dao1 = new LocalDAO();
         dao1.remover(5);
-        
+
     }
-    
+
+    @Test
+    public void consultarLocal() {
+
+        Local l1 = new Local();
+        LocalDAO da1 = new LocalDAO();
+        List<IEntidade> locais = new ArrayList<>();
+
+        locais = da1.consultarLocal("");
+
+        for (IEntidade obj : locais) {
+            Local local = (Local) obj;
+            System.out.println(local.getId() + " - " + local.getDescricao());
+        }
+
+    }
+
 }
