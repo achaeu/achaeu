70ae81f9b6a9ec770d9f3db3a199bab93a3a743d
IgorColetoBueno <igor_mcb@hotmail.com>
2018-06-18 20:22:24 -0400

    Correção na model HORARIO_LOCAL

--- br.com.Achaeu/src/model/HorarioLocal.java
+++ br.com.Achaeu/src/model/HorarioLocal.java
@@ -11,18 +11,21 @@ import java.sql.Time;
  *
  * @author igor-bueno
  */
-public class HorarioLocal extends IEntidade{
+public class HorarioLocal extends IEntidade {
+
     private Time entrada1;
     private Time saida1;
     private Time entrada2;
     private Time saida2;
     private Integer funcionamento;
     private Integer diaSemana;
+    private Integer idLocal;
+    private Local local;
 
     public HorarioLocal() {
     }
 
-    public HorarioLocal(Integer id, Time entrada1, Time saida1, Time entrada2, Time saida2, Integer funcionamento, Integer diaSemana) {
+    public HorarioLocal(Time entrada1, Time saida1, Time entrada2, Time saida2, Integer funcionamento, Integer diaSemana, Integer idLocal, Local local, Integer id) {
         super(id);
         this.entrada1 = entrada1;
         this.saida1 = saida1;
@@ -30,6 +33,8 @@ public class HorarioLocal extends IEntidade{
         this.saida2 = saida2;
         this.funcionamento = funcionamento;
         this.diaSemana = diaSemana;
+        this.idLocal = idLocal;
+        this.local = local;
     }
 
     public Time getEntrada1() {
@@ -80,8 +85,24 @@ public class HorarioLocal extends IEntidade{
         this.diaSemana = diaSemana;
     }
 
+    public Integer getIdLocal() {
+        return idLocal;
+    }
+
+    public void setIdLocal(Integer idLocal) {
+        this.idLocal = idLocal;
+    }
+
+    public Local getLocal() {
+        return local;
+    }
+
+    public void setLocal(Local local) {
+        this.local = local;
+    }
+
     @Override
     public String toString() {
-        return "HorarioLocal{" + "entrada1=" + entrada1 + ", saida1=" + saida1 + ", entrada2=" + entrada2 + ", saida2=" + saida2 + ", funcionamento=" + funcionamento + ", diaSemana=" + diaSemana + '}';
-    }
+        return "HorarioLocal{" + "entrada1=" + entrada1 + ", saida1=" + saida1 + ", entrada2=" + entrada2 + ", saida2=" + saida2 + ", funcionamento=" + funcionamento + ", diaSemana=" + diaSemana + ", idLocal=" + idLocal + ", local=" + local + '}';
+    }    
 }
