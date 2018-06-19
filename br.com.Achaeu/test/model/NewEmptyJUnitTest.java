bfc1161c56df6a822208a66036083c7ee9e3dec8
Jonathan Lazzaro <jonathan.lazaro1@gmail.com>
2018-06-19 17:27:35 -0400

    Buscando caminho do relatório.

--- br.com.Achaeu/test/model/NewEmptyJUnitTest.java
+++ br.com.Achaeu/test/model/NewEmptyJUnitTest.java
@@ -6,8 +6,8 @@
 package model;
 
 import DAO.ConnectionManager;
+import java.io.File;
 import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -16,41 +16,43 @@ import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.view.JasperViewer;
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 /**
  *
  * @author Rodrigo
  */
 public class NewEmptyJUnitTest {
-    
+
     public NewEmptyJUnitTest() {
     }
-    
+
     @Test
-    public void testeImp(){
-        
+    public void testeImp() {
+
         Connection conexao;
         try {
             conexao = ConnectionManager.getConexao();
         } catch (SQLException ex) {
-             conexao = null;
+            conexao = null;
             Logger.getLogger(NewEmptyJUnitTest.class.getName()).log(Level.SEVERE, null, ex);
         }
+        String src = "reports/Silhouette_Landscape.jasper";
+        ClassLoader classLoader = new NewEmptyJUnitTest().getClass().getClassLoader();
+        File file = new File(classLoader.getResource(src).getFile());
+        String absolutePath = file.getAbsolutePath();
+        //String src = "br.com.Achaeu/src/reports/Silhouette_Landscape.jasper";
 
-        String src = "br.com.Achaeu/src/reports/Silhouette_Landscape.jasper";
-        
         JasperPrint jasper = null;
-        
+
         try {
-            jasper = JasperFillManager.fillReport(src, null, conexao);
+            jasper = JasperFillManager.fillReport(absolutePath, null, conexao);
         } catch (JRException ex) {
             Logger.getLogger(NewEmptyJUnitTest.class.getName()).log(Level.SEVERE, null, ex);
         }
-     
+
         JasperViewer view = new JasperViewer(jasper);
         view.setVisible(true);
-        
+
     }
-    
+
 }
